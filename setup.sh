d65694eb145b08b549871d6ac7e0936274a54fb5
dabao1955 <79307765+dabao1955@users.noreply.github.com>
2023-04-29 12:23:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -84,8 +84,8 @@ echo 即将安装flash-tool-linux到/usr/share/flash-linux-tool
 read -s -n1 -p "按任意键继续 ... "
 echo 正在安装
 sudo git clone https://github.com/dabao1955/flash-tool-linux /usr/share >log.txt || echo 安装失败 ; exit 1 
-sudo ln -s /usr/share/flash-linux-tool/menu /usr/local/bin/flash-tool-linux || echo 安装失败 ; exit 1 
-sudo ln -s /usr/share/flash-linux-tool/menu /usr/local/bin/flt || echo 安装失败 ; exit 1 
+sudo ln -s /usr/share/flash-linux-tool/init /usr/local/bin/flash-tool-linux || echo 安装失败 ; exit 1 
+sudo ln -s /usr/share/flash-linux-tool/init /usr/local/bin/flt || echo 安装失败 ; exit 1 
 sudo chmod 755 /usr/local/bin/flash-linux-tool || echo 安装失败 ; exit 1 
 sudo chmod 755 /usr/local/bin/flt || echo 安装失败 ; exit 1 
 echo 安装完成，可使用flt或者flash-linux-tool来启动 
